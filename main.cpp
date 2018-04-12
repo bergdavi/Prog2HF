e77973e8237d2b661f3d8996331c4293e6830c08
bergdavi <berghammer.david@gmail.com>
2018-04-12 11:38:29 +0200

    Added test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "Circle.h"
 #include "Triangle.h"
 #include "Polygon.h"
+#include "gtest_lite.h"
 
 using namespace std;
 
@@ -57,6 +58,13 @@ int main()
     svgFile << "</svg>" << endl;
 
     svgFile.close();
+
+    TEST(Test1, emptyStr) {
+      Shape* s = new Square(Vector2(0,0), Vector2(1,1));
+      EXPECT_EQ(4.0, s->getArea()) << "Nem jo a terulet" << endl;
+      EXPECT_EQ(8.0, s->getCircumference()) << "Nem jo a Kerulet" << endl;
+
+    } ENDM
 
 
 
